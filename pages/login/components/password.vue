ecb29c1390b65ef55ce2946521904990738f9e2d
lotjol <botue@sina.com>
2023-08-08 12:03:43 +0800

    feat(login): login by password

--- pages/login/components/password.vue
+++ pages/login/components/password.vue
@@ -1,12 +1,57 @@
-<script setup></script>
+<script setup>
+  import { ref, computed } from 'vue'
+
+  // 表单类型是否为密码
+  const isPassword = ref(true)
+  // 是否同意协议
+  const agreementChecked = ref(false)
+  // uni-form 组件
+  const formRef = ref()
+  // 表单数据
+  const formData = ref({
+    mobile: '',
+    password: '',
+  })
+  // 表单验证规则
+  const formRules = {
+    mobile: {
+      rules: [
+        { required: true, errorMessage: '请填写手机号码' },
+        { pattern: '^1\d{10}$', errorMessage: '手机号码格式不正确' },
+      ],
+    },
+    password: {
+      rules: [
+        { required: true, errorMessage: '请输入密码' },
+        { pattern: '^\\d{6}$', errorMessage: '密码格式不正确' },
+      ],
+    },
+  }
+  // 表单提交
+  async function onFormSubmit() {
+    try {
+      // 验证表单数据，通过后可以获取表单数据
+      const formData = await formRef.value.validate()
+      // 提交数据
+    } catch (err) {
+      console.log(err)
+    }
+  }
+</script>
 
 <template>
-  <uni-forms class="login-form" ref="form">
+  <uni-forms
+    class="login-form"
+    :model="formData"
+    :rules="formRules"
+    ref="formRef"
+  >
     <uni-forms-item name="mobile">
       <input
         type="text"
         placeholder="请输入手机号"
         class="uni-input-input"
+        v-model="formData.mobile"
         placeholder-style="color: #C3C3C5"
       />
     </uni-forms-item>
@@ -15,22 +60,33 @@
         type="text"
         placeholder="请输入密码"
         class="uni-input-input"
+        v-model="formData.passsword"
+        :password="isPassword"
         placeholder-style="color: #C3C3C5"
       />
-      <view class="uni-icon">
-        <uni-icons v-if="true" size="18" color="#6F6F6F" type="eye-slash" />
+      <view @click="isPassword = !isPassword" class="uni-icon">
+        <uni-icons
+          v-if="isPassword"
+          size="18"
+          color="#6F6F6F"
+          type="eye-slash"
+        />
         <uni-icons v-else size="18" color="#6F6F6F" type="eye" />
       </view>
     </uni-forms-item>
     <view class="agreement">
-      <radio :checked="false" color="#16C2A3" />
+      <radio
+        @click="agreementChecked = !agreementChecked"
+        :checked="agreementChecked"
+        color="#16C2A3"
+      />
       我已同意
       <text class="link">用户协议</text>
       及
       <text class="link">隐私协议</text>
     </view>
 
-    <button class="uni-button">登 录</button>
+    <button @click="onFormSubmit" class="uni-button">登 录</button>
     <navigator class="uni-navigator" url=" ">忘记密码？</navigator>
   </uni-forms>
 </template>
